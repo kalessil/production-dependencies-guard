507102b5aed0241d3d49954159ec1fb9103aa4fa
Vladimir Reznichenko <kalessil@gmail.com>
2019-04-28 17:34:02 +0200

    WIP, populating the repository

--- src/Repository.php
+++ src/Repository.php
@@ -4,7 +4,51 @@ namespace Kalessil\Composer\Plugins\ProductionDependenciesGuard;
 
 final class Repository
 {
+    private $vendors  = [
+        'phpunit/',
+        'codeception/',
+        'behat/',
+        'phpspec/'
+    ];
+
+    private $packages = [
+        'kalessil/production-dependencies-guard',
+        'roave/security-advisories',
+
+        /* PhpUnit-extensions and tooling */
+        'johnkary/phpunit-speedtrap',
+        'brianium/paratest',
+        'mybuilder/phpunit-accelerator',
+        'codedungeon/phpunit-result-printer',
+        'spatie/phpunit-watcher',
+        
+        /* Frameworks components and tooling */
+        'symfony/phpunit-bridge',
+        'symfony/debug',
+        'symfony/var-dumper',
+        'symfony/maker-bundle',
+        'zendframework/zend-test',
+        'zendframework/zend-debug',
+        'yiisoft/yii2-gii',
+        'yiisoft/yii2-debug',
+        'orchestra/testbench',
+        'barryvdh/laravel-debugbar',
+
+        /* more dev-packages  */
+        'humbug/humbug',
+        'infection/infection',
+        'mockery/mockery',
+        'satooshi/php-coveralls',
+        'mikey179/vfsStream',
+        'filp/whoops',
+    ];
+
+    private function containsVendor(string $dependency): bool {
+        $callback = static function (string $vendor) use ($dependency): bool { return stripos($dependency, $vendor) === 0; };
+        return array_filter($this->vendors, $callback) === [];
+    }
+
     public function contains(string $dependency): bool {
-        return $dependency === 'kalessil/production-dependencies-guard';
+        return \in_array(strtolower($dependency), $this->packages, true) || $this->containsVendor($dependency);
     }
 }
